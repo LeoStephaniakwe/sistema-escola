5c08dc47436a42bdf1c73aa38b838dc5e0adcf96
Leonardo Stephaniakwe <leonardomoreira050@gmail.com>
2018-05-21 11:24:58 -0300

    Criado Arquivo

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,13 +1,24 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
- * @author Alunos
+ * @author Leonardo Moreira e Daiana Marques
  */
 public class Aluno {
-    
+//solicitação de comando ao usuario
+    public static void main(String[] args) {
+
+        String cadastro = Integer.parseInt(JOptionPane.showMessageDialog(null,
+                "Escolha o que voce deseja fazer"));
+        
+        String[] alunos = new String [100];
+        
+
+    }
 }
