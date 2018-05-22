c3d752a4c5edcf37b6c558ad86cc5488fa68c585
Alunos <Alunos@M729>
2018-05-22 10:14:41 -0300

    Finalizado Classe

--- src/Professores.java
+++ src/Professores.java
@@ -1,3 +1,6 @@
+//CADASTRO PROFESSORES
+
+import javax.swing.JOptionPane;
 
 public class Professores {
 
@@ -17,67 +20,93 @@ public class Professores {
     String[] formacao = new String[100];
     double[] salario = new double[100];
     int professores = 0;
+    private int i;
 
-}
-public void cadastrar() {
+    public void cadastrar() {
         solicitarInformacao(professores++);
         professores++;
-}
+    }
 
-        public void editar(){
-String busca = JOptionPane.showInputDialog(null,"digite o nome para edição:");
-for(int i = 0; i< professorees; i++){
-if(nomes[i].equals(busca)){
-    solicitarInformacao++;
-}
-}
+    public void editar() {
+        String busca = JOptionPane.showInputDialog(null, "digite o nome para edição:");
+        for (int i = 0; i < professores; i++) {
+            if (nomes[i].equals(busca)) {
+                solicitarInformacao(i);
 
-}
-public void apresentarInfo(int i){
-JOptionPane.showMessageDialog(null,
-                "Nome: " + nomes[i]
-                + "\nIdade: " + idades[i]
-                + "\nCPF: " + cpfs[i]
-                + "\nEstado: " + estados[i]
-                + "\nCidade: " + cidades[i]
-                + "\nBairro: " + bairros[i]
-                + "\nLogradouro: " + logradouros[i]
-                + "\nCEP: " + ceps[i]
-                + "\nNumero: " + numeros[i]
-                + "\nComplemento:" + complementos[i]
-                + "\nMatéria" + materia[i]
-                + "Carga Horária"
-                + "Formação" + formacao[i]
-                + "Salario" + salario[i]);
+            }
+        }
 
-}
-public void buscarPeloNome(){
-String busca = JOptionPane.showInputDialog(null, 
-"Digite o nome  parcial que deseja buscar:"):
-}
-pulic void contabilizarPeloNome(){
-String busca = JOptionPane.showInputDialog(null, 
-"Digite o nome para busca");
-int quantidadeDeRegistros = 0;
-for(int i = 0 ; i < professores; i++){
-if(nomes[i].contains(busca)){
-quantidadeDeRegistros++;
-}
-}
-}
+    }
 
-public void buscarPeloCpf(){
-String cpfBusca = JOptionPane.showInputDialog("Digite o CPF que desejas buscar").replace(".","").replace("-","");
-for (int i = 0; i <professores; i++){
-if(cpfs[i].equals(cpfBusca)){
-apresentarInformacao[i];
-}
-}
-}
-public void listar() {
+    public void apresentarInfo(int i) {
+        JOptionPane.showMessageDialog(null,
+                "Salario"
+                        + salario[i] + "Nome: " + nomes[i]
+                        + "\nIdade: " + idades[i]
+                        + "\nCPF: " + cpfs[i]
+                        + "\nEstado: " + estados[i]
+                        + "\nCidade: " + cidades[i]
+                        + "\nBairro: " + bairros[i]
+                        + "\nLogradouro: " + logradouros[i]
+                        + "\nCEP: " + ceps[i]
+                        + "\nNumero: " + numeros[i]
+                        + "\nComplemento:" + complementos[i]
+                        + "\nMatéria" + materia[i]
+                        + "Carga Horária" + cargaHoraria[i]
+                        + "Formação" + formacao[i]);
+
+    }
+
+    public void buscarPeloNome() {
+        String busca = JOptionPane.showInputDialog(null,
+                "Digite o nome  parcial que deseja buscar:");
+    }
+
+    public void contabilizarPeloNome() {
+        String busca = JOptionPane.showInputDialog(null,
+                "Digite o nome para busca");
+        int quantidadeDeRegistros = 0;
+        for (int i = 0; i < professores; i++) {
+            if (nomes[i].contains(busca)) {
+                quantidadeDeRegistros++;
+            }
+        }
+    }
+
+    public void buscarPeloCpf() {
+        String cpfBusca = JOptionPane.showInputDialog("Digite o CPF que desejas buscar").replace(".", "").replace("-", "");
+        for (int i = 0; i < professores; i++) {
+            if (cpfs[i].equals(cpfBusca)) {
+                apresentarInfo(i);
+            }
+        }
+    }
+
+    public void listar() {
         String texto = "";
         for (int i = 0; i < professores; i++) {
             texto += nomes[i] + "  " + cpfs[i] + "\n";
         }
         JOptionPane.showMessageDialog(null, texto);
     }
+
+    public void solicitarInformacao(int posicao) {
+        nomes[posicao] = JOptionPane.showInputDialog("Digite o nome:");
+        idades[posicao] = Integer.parseInt(JOptionPane.showInputDialog(nomes[posicao] + " Digite a sua idade"));
+        sexos[posicao] = JOptionPane.showInputDialog((nomes[posicao] + " Digite o seu sexo:")).charAt(0);
+        cpfs[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o seu CPF").replace(".", "").replace("-", "");
+        estados[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o seu estado").trim().toUpperCase();
+        cidades[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite a sua cidade:");
+        logradouros[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o seu logradouro:");
+        ceps[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o seu cep:");
+        numeros[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o numero");
+        complementos[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o complemento");
+        materia[posicao] = JOptionPane.showInputDialog(nomes[posicao] + "Qual a materia lescionada?");
+        cargaHoraria[i] = Double.parseDouble(JOptionPane.showInputDialog(nomes[posicao] + "Carga horaria estimada"));
+        formacao[i] = JOptionPane.showInputDialog(nomes[posicao] + "Méritos?");
+        salario[i] = Double.parseDouble(JOptionPane.showInputDialog(nomes[posicao] + "Salario:"));
+
+    }
+}
+
+    
