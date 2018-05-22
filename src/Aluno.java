e86b520185f2ce7f708dbebd4e08e2c96b694966
Alunos <Alunos@M729>
2018-05-22 10:20:57 -0300

    Finalizado Classe

--- src/Aluno.java
+++ src/Aluno.java
@@ -17,26 +17,26 @@ public class Aluno {
     String[] complementos = new String[100];
     double[] notas = new double[100];
     int[] faltas = new int[100];
-    String[] obsevacoesGerais = new String[100];
+    String[] observacoesGerais = new String[100];
     int aluno = 0;
 
-}
-public void cadastrar() {
+    public void cadastrar() {
         solicitarInformacao(aluno++);
         aluno++;
-}
+    }
 
-        public void editar(){
-String busca = JOptionPane.showInputDialog(null,"digite o nome para edição:");
-for(int i = 0; i< aluno; i++){
-if(nomes[i].equals(busca)){
-    solicitarInformacao++;
-}
-}
+    public void editar() {
+        String busca = JOptionPane.showInputDialog(null, "digite o nome para edição:");
+        for (int i = 0; i < aluno; i++) {
+            if (nomes[i].equals(busca)) {
+                solicitarInformacao(i);
+            }
+        }
 
-}
-public void apresentarInfo(int i){
-JOptionPane.showMessageDialog(null,
+    }
+
+    public void apresentarInfo(int i) {
+        JOptionPane.showMessageDialog(null,
                 "Nome: " + nomes[i]
                 + "\nIdade: " + idades[i]
                 + "\nCPF: " + cpfs[i]
@@ -47,42 +47,47 @@ JOptionPane.showMessageDialog(null,
                 + "\nCEP: " + ceps[i]
                 + "\nNumero: " + numeros[i]
                 + "\nComplemento:" + complementos[i]
-                + "\nNotas:" + notas[i];
-                + "\nFaltas" + faltas[i];
-                + "\nObservações:" + observacoesGerais[i];
+                + "\nNotas:" + notas[i]
+                + "\nFaltas" + faltas[i]
+                + "\nObservações:" + observacoesGerais[i]);
 
-}
-public void buscarPeloNome(){
-String busca = JOptionPane.showInputDialog(null,
-"Digite o nome  parcial que deseja buscar:"):
-}
-pulic void contabilizarPeloNome(){
-String busca = JOptionPane.showInputDialog(null,
-"Digite o nome para busca");
-int quantidadeDeRegistros = 0;
-for(int i = 0 ; i < aluno; i++){
-if(nomes[i].contains(busca)){
-quantidadeDeRegistros++;
-}
-}
-}
+    }
 
-public void buscarPeloCpf(){
-String cpfBusca = JOptionPane.showInputDialog("Digite o CPF que desejas buscar").replace(".","").replace("-","");
-for (int i = 0; i <aluno; i++){
-if(cpfs[i].equals(cpfBusca)){
-apresentarInformacao[i];
+    public void buscarPeloNome() {
+        String busca = JOptionPane.showInputDialog(null,
+                "Digite o nome  parcial que deseja buscar:");
+    
+    
 }
-}
-}
-public void listar() {
+public void contabilizarPeloNome() {
+        String busca = JOptionPane.showInputDialog(null,
+                "Digite o nome para busca");
+        int quantidadeDeRegistros = 0;
+        for (int i = 0; i < aluno; i++) {
+            if (nomes[i].contains(busca)) {
+                quantidadeDeRegistros++;
+            }
+        }
+    }
+
+    public void buscarPeloCpf() {
+        String cpfBusca = JOptionPane.showInputDialog("Digite o CPF que desejas buscar").replace(".", "").replace("-", "");
+        for (int i = 0; i < aluno; i++) {
+            if (cpfs[i].equals(cpfBusca)) {
+                apresentarInfo(i);
+            }
+        }
+    }
+
+    public void listar() {
         String texto = "";
-        for (int i = 0; i < professores; i++) {
+        for (int i = 0; i < aluno; i++) {
             texto += nomes[i] + "  " + cpfs[i] + "\n";
         }
         JOptionPane.showMessageDialog(null, texto);
     }
- public void solicitarInformacao(int posicao) {
+
+    public void solicitarInformacao(int posicao) {
         nomes[posicao] = JOptionPane.showInputDialog("Digite o nome:");
         idades[posicao] = Integer.parseInt(JOptionPane.showInputDialog(nomes[posicao] + " Digite a sua idade"));
         sexos[posicao] = JOptionPane.showInputDialog((nomes[posicao] + " Digite o seu sexo:")).charAt(0);
@@ -93,4 +98,6 @@ public void listar() {
         ceps[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o seu cep:");
         numeros[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o numero");
         complementos[posicao] = JOptionPane.showInputDialog(nomes[posicao] + " Digite o complemento");
+    
     }
+}
